e1d7ce14f3f0bd981088ddb7c7f4dec12828376b
rm <sirycity2018@gmail.com>
2019-07-27 23:44:31 +0900

    セッション2/3 終了

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '実験',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: '実験です。' },
       
       { name: "robots", content: "noindex" },
       { name: "robots", content: "nofollow" },
--- store/progress.js
+++ store/progress.js
@@ -7,7 +7,7 @@ export const state = () =>
      *  index       最初の画面
      *  consent     実験に同意していない
      *  operation   実験に同意し、操作を説明
-     *  experiment  実験に同意し、実験が開始した
+     *  measurement  実験に同意し、実験が開始した
      *  finished    実験終了
      *  thanks      実験結果送信済み
      */
@@ -18,35 +18,75 @@ export const state = () =>
 export const actions = {
   checkAndRedirectOrNot({commit, rootState}, currentPageType){
     
+    // 直前のページタイプを取得
     const previousPageType = rootState.progress.pageType
 
-    console.dir(currentPageType)
-return 
-    // 初見なら必ず初期ページに戻る
+    // 外部から来た場合は必ずindexに戻る
     if(previousPageType === 'directly'){
       this.$router.push('/')
+      return
     }
 
+    // 内部からならページタイプを更新
+    commit('updatePageType', currentPageType)
 
-
+    // 最初の3つに飛んだら
     if(
       currentPageType === 'index'
       || currentPageType === 'consent'
       || currentPageType === 'operation'
     ){
 
-    }
-    
-    
-    if(currentPageType === 'experiment'){
-      if(previousPageType === 'operation'){
+      // measurementからなら実験中なので戻す
+      if(previousPageType === 'measurement'){
+        this.$router.push('measurement')
+        return 
+      }
+
+      // finishedからならまだ実験結果送信してないので戻す
+      if(previousPageType === 'finished'){
+        this.$router.push('finished')
+        return 
+      }
+
+      // thanksからならおそらく実験2回目やりたい人の誤作動なのでトップに戻す
+      if(previousPageType === 'thanks'){
+        this.$router.push('/')
         return
       }
-      else if(previousPageType === 'operation'){
 
-      }
+      // 他は特に何もしない
+      return
     }
     
+    // 実験中のページに飛んだら
+    if(currentPageType === 'measurement'){
+
+      // operationか
+      // measurementからなら順当な流れなので何もしない
+      if(
+        previousPageType === 'operation'
+        || previousPageType === 'measurement'
+      ){
+        this.$router.push('measurement')
+        return 
+      }
+
+      // finishedからならまだ実験結果送信してないので戻す
+      if(previousPageType === 'finished'){
+        this.$router.push('finished')
+        return 
+      }
+
+      // 他はトップに戻す
+      else {
+        this.$router.push('/')
+        return
+      }
+
+    }
+    
+    // 実験完了のページに飛んだら
     if(currentPageType === 'finished'){
       
     }
@@ -59,6 +99,10 @@ return
 
 export const mutations = {
   updatePageType(state, pageType){
-    
+    state.pageType = pageType
+  },
+
+  resetResults(_, rootState){
+    console.dir(rootState.experiment.results)
   }
 }
