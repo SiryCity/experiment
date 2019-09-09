ffdad5633b16ad1066366b0468532f2e32b36e7c
rm <sirycity2018@gmail.com>
2019-09-09 20:47:15 +0900

    コメント追加中

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<!--
+● index       最初に来るところ、「実験を始める」程度のことだけ書く
+  consent     実験の同意をとる
+  operation   実験の操作方法を教える
+  measurement 実験開始 この画面でデータの計測を行う
+  finished    終わったことを伝える 計測データの送信ボタンを置く
+  thanks      お礼を言う もう一度計測するボタンを置く
+-->
 <template lang="pug">
   div.index
     h1 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
