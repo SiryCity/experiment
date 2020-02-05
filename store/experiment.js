60f217ea7c3b48a550fdb3c53a8c57a6ebfb7c9a
rm <sirycity2018@gmail.com>
2020-02-05 12:37:03 +0900

    検証前

--- store/experiment.js
+++ store/experiment.js
@@ -1,26 +1,25 @@
 export const actions = {
   // 「実験をはじめる」を押した時
-  init({commit}){
-    commit('setInitialState')
-    commit('setNextState')
+  init({ commit }) {
+    commit("setInitialState");
+    commit("setNextState");
   },
   // 画面をタッチした時
-  touched({commit}, e){
-    commit('resisterResult', e)
-    commit('setNextState')
+  touched({ commit }, e) {
+    commit("resisterResult", e);
+    commit("setNextState");
   }
-}
+};
 
 // 何回タッチしたらゲームが終了か
-const LIMIT = 100
+const LIMIT = 100;
 
 // 座標を何分割するか
-const DIVISIONS = 21
+const DIVISIONS = 21;
 
 export const mutations = {
-
   // 結果をresults[]に追加
-  resisterResult(state, e){
+  resisterResult(state, e) {
     state.results.push({
       // ユニークID
       u: state.uniqueId,
@@ -44,75 +43,79 @@ export const mutations = {
       ty: e.pageY,
 
       // 経過時間(秒)
-      t: ~~(((new Date().getTime() + '').slice(5)|0) - (~~(state.previousTime * 1000) / 1000) * 1000) / 1000,
+      t:
+        ~~(
+          ((new Date().getTime() + "").slice(5) | 0) -
+          (~~(state.previousTime * 1000) / 1000) * 1000
+        ) / 1000,
 
       // HSL
       h: state.hue,
       s: state.saturation,
       l: state.lightness,
-      
+
       //v2はhueのバグ修正(256~360)
       //v3はhue全部(0~360)
-      v: 3,
-    })
+      v: 3
+    });
   },
 
-  setDominance(state, dominance){
-
+  setDominance(state, dominance) {
     // 利き手を決定
-    state.dominance = dominance
+    state.dominance = dominance;
   },
 
-  setInitialState(state){
-
+  setInitialState(state) {
     // ユニークIDを決定
-    state.uniqueId = ~~(Math.random() * 1000000)
+    state.uniqueId = ~~(Math.random() * 1000000);
 
     // 画面の幅と高さを決定
-    state.outerWidth = window.outerWidth
-    state.outerHeight = window.outerHeight
-
+    state.outerWidth = window.outerWidth;
+    state.outerHeight = window.outerHeight;
   },
-  
-  setNextState(state){
 
+  setNextState(state) {
     // 指定された回数タップしたら実験を終了
-    if(state.times > LIMIT - 1){
-      state.times = 0
-      this.$router.push('finished')
-      return
+    if (state.times > LIMIT - 1) {
+      state.times = 0;
+      this.$router.push("finished");
+      return;
     }
 
     // 回数を加算
-    state.times++
+    state.times++;
 
     // 次の座標をランダムに更新
-    state.translateX = state.outerWidth / DIVISIONS * ~~(Math.random() * DIVISIONS)
-    state.translateY = state.outerHeight / DIVISIONS * ~~(Math.random() * DIVISIONS)
-    
+    state.translateX =
+      (state.outerWidth / DIVISIONS) * ~~(Math.random() * DIVISIONS);
+    state.translateY =
+      (state.outerHeight / DIVISIONS) * ~~(Math.random() * DIVISIONS);
+
     // 次の色をランダムに更新
-    state.hue = ~~(Math.random() * 361)
-    state.saturation = ~~(Math.random() * 101)
-    state.lightness = ~~(Math.random() * 101)
+    state.hue = ~~(Math.random() * 361);
+    state.saturation = ~~(Math.random() * 101);
+    state.lightness = ~~(Math.random() * 101);
 
     // 経過時間を追加
-    state.previousTime += ~~(((new Date().getTime() + '').slice(5)|0) - (~~(state.previousTime * 1000) / 1000) * 1000) / 1000
-
+    state.previousTime +=
+      ~~(
+        ((new Date().getTime() + "").slice(5) | 0) -
+        (~~(state.previousTime * 1000) / 1000) * 1000
+      ) / 1000;
   }
-}
+};
 
-export const state = () =>
-  ({
-    uniqueId: null,
-    dominance: null,
-    times: 0,
-    outerWidth: 0,
-    outerHeight: 0,
-    translateX: 0,
-    translateY: 0,
-    previousTime: 0,
-    hue: 0,
-    saturation: 0,
-    lightness: 100,
-    results: [],
-  })
+export const state = () => ({
+  uniqueId: null,
+  dominance: null,
+  times: 0,
+  outerWidth: 0,
+  outerHeight: 0,
+  translateX: 0,
+  translateY: 0,
+  previousTime: 0,
+  hue: 0,
+  saturation: 0,
+  lightness: 100,
+  results: []
+});
--- store/firebase.js
+++ store/firebase.js
@@ -2,259 +2,204 @@
  * 実験結果をfirestoreに保存
  */
 
-import firebase from 'firebase/app'
-import Chart from 'chart.js'
-
-
+import firebase from "firebase/app";
+import Chart from "chart.js";
 
 export const actions = {
-
-  async insert({rootState}){
-    
+  async insert({ rootState }) {
     // 実験結果の巨大なJSONを代入
-    const RESULTS = rootState.experiment.results
+    const RESULTS = rootState.experiment.results;
 
-    firebase.firestore().collection('results').add({
-      ... RESULTS
-    })
+    firebase
+      .firestore()
+      .collection("results")
+      .add({
+        ...RESULTS
+      });
 
-    console.dir(RESULTS)
+    console.dir(RESULTS);
   },
 
-  async select({commit}){
+  async select({ commit }) {
+    console.dir("clicked");
 
-    console.dir('clicked')
+    const rawResults = await firebase
+      .firestore()
+      .collection("results")
+      .get();
 
-    const rawResults = await firebase.firestore().collection('results').get()
+    let results = [];
 
-    let results = []
+    rawResults.forEach(doc => results.push(doc.data()));
 
-    rawResults.forEach(doc => results.push(doc.data()))
-
-    commit('renderGraph', results)
+    commit("renderGraph", results);
   }
-}
-
+};
 
 export const mutations = {
-  renderGraph(_, rawResults){
+  renderGraph(_, rawResults) {
+    const results = makeResultsToInALine(rawResults)
+      .map(v => ({
+        ...v,
+        t: v.t < 0 ? 0 : v.t
+      }))
+      .filter(v => v.n <= 100 && v.n > 0);
 
-    const results = makeResultsToInALine(rawResults).map(v =>
-        ({
-          ... v,
-          t: v.t < 0 ? 0 : v.t
-        })
-      ).filter(v => v.n <= 100 && v.n > 0)
+    const ver = v => (v ? v : 1);
 
-    const ver = v =>
-      v ? v : 1
-    
-    console.dir(rawResults)
-    console.dir(results)
-    
-    console.info(results.filter(e => e.v && e.v === 2 ).length / 100)
+    console.dir(rawResults);
+    console.dir(results);
+
+    console.info(results.filter(e => e.v && e.v === 2).length / 100);
     console.info([
-      results.filter(e => e.d === 'right' ).length,
-      results.filter(e => e.d === 'left' ).length,
-    ])
+      results.filter(e => e.d === "right").length,
+      results.filter(e => e.d === "left").length
+    ]);
 
-    ~chart
-    `scatter--n-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--n-time``scatter`({
+      datasets: [
         {
-          label: 'タッチ回数と所要時間',
-          data: results.map(({n, t}) =>
-            ({
-              x: n,
-              y: t > 2 ? 2 : t
-            })
-          )
+          label: "タッチ回数と所要時間",
+          data: results.map(({ n, t }) => ({
+            x: n,
+            y: t > 2 ? 2 : t
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--n-dist`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--n-dist``scatter`({
+      datasets: [
         {
-          label: 'タッチ回数とタッチ誤差',
-          data: results.map(({n, px, py, tx, ty}) =>
-            ({
-              x: n,
-              y: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
-              ? 600
-              : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2)
-            })
-          )
+          label: "タッチ回数とタッチ誤差",
+          data: results.map(({ n, px, py, tx, ty }) => ({
+            x: n,
+            y:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+                ? 600
+                : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2)
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--h-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--h-time``scatter`({
+      datasets: [
         {
-          label: '色相と所要時間',
-          data: results.map(({h, t, v}) =>
-            ({
-              x: ver(v) === 1
-              ? ~~(h / 255 * 360)
-              : ver(v) === 2
-              ? ~~((h - 256) / 104 * 360)
-              : h,
-              y: t > 2 ? 2 : t
-            })
-          )
+          label: "色相と所要時間",
+          data: results.map(({ h, t, v }) => ({
+            x:
+              ver(v) === 1
+                ? ~~((h / 255) * 360)
+                : ver(v) === 2
+                ? ~~(((h - 256) / 104) * 360)
+                : h,
+            y: t > 2 ? 2 : t
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--l-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--l-time``scatter`({
+      datasets: [
         {
-          label: '明度と所要時間',
-          data: results.map(({l, t}) =>
-            ({
-              x: l,
-              y: t > 2 ? 2 : t
-            })
-          )
+          label: "明度と所要時間",
+          data: results.map(({ l, t }) => ({
+            x: l,
+            y: t > 2 ? 2 : t
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--s-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--s-time``scatter`({
+      datasets: [
         {
-          label: '彩度と所要時間',
-          data: results.map(({s, t}) =>
-            ({
-              x: s,
-              y: t > 2 ? 2 : t
-            })
-          )
+          label: "彩度と所要時間",
+          data: results.map(({ s, t }) => ({
+            x: s,
+            y: t > 2 ? 2 : t
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--dist-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--dist-time``scatter`({
+      datasets: [
         {
-          label: 'タッチ誤差と所要時間',
-          data: results.map(({px, py, tx, ty, t}) =>
-            ({
-              x: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
-              ? 600
-              : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
-              y: t > 5 ? 5 : t
-            })
-          )
-        }
-      ]
-    })
-    
-    ~chart
-    `scatter--dist-h`
-    `scatter`
-    ({
-      datasets:
-      [
-        {
-          label: '色相とタッチ誤差',
-          data: results.map(({px, py, tx, ty, h, v})=>
-            ({
-              y: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+          label: "タッチ誤差と所要時間",
+          data: results.map(({ px, py, tx, ty, t }) => ({
+            x:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
                 ? 600
                 : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
-              x: ver(v) === 1
-                ? ~~(h / 255 * 360)
-                : ver(v) === 2
-                ? ~~((h - 256) / 104 * 360)
-                : h,
-            })
-          )
+            y: t > 5 ? 5 : t
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--dist-l`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--dist-h``scatter`({
+      datasets: [
         {
-          label: '明度とタッチ誤差',
-          data: results.map(({px, py, tx, ty, l})=>
-            ({
-              y: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
-              ? 600
-              : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
-              x: l
-            })
-          )
+          label: "色相とタッチ誤差",
+          data: results.map(({ px, py, tx, ty, h, v }) => ({
+            y:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+                ? 600
+                : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
+            x:
+              ver(v) === 1
+                ? ~~((h / 255) * 360)
+                : ver(v) === 2
+                ? ~~(((h - 256) / 104) * 360)
+                : h
+          }))
         }
       ]
-    })
-    
-    ~chart
-    `scatter--dist-s`
-    `scatter`
-    ({
-      datasets:
-      [
+    });
+
+    ~chart`scatter--dist-l``scatter`({
+      datasets: [
         {
-          label: '彩度とタッチ誤差',
-          data: results.map(({px, py, tx, ty, s})=>
-            ({
-              y: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
-              ? 600
-              : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
-              x: s
-            })
-          )
+          label: "明度とタッチ誤差",
+          data: results.map(({ px, py, tx, ty, l }) => ({
+            y:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+                ? 600
+                : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
+            x: l
+          }))
         }
       ]
-    })
-    
-    ~chart
-    `scatter--px-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    });
+
+    ~chart`scatter--dist-s``scatter`({
+      datasets: [
         {
-          label: 'タッチ箇所(x軸)と所要時間',
-          data: results.map(({dx, px, t}) =>
-            ({
-              x: (100 / dx * px) + ~~(Math.random() * 5),
-              y: t > 5 ? 5 : t
-            })
-          )
+          label: "彩度とタッチ誤差",
+          data: results.map(({ px, py, tx, ty, s }) => ({
+            y:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+                ? 600
+                : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
+            x: s
+          }))
         }
       ]
-    })
+    });
+
+    ~chart`scatter--px-time``scatter`({
+      datasets: [
+        {
+          label: "タッチ箇所(x軸)と所要時間",
+          data: results.map(({ dx, px, t }) => ({
+            x: (100 / dx) * px + ~~(Math.random() * 5),
+            y: t > 5 ? 5 : t
+          }))
+        }
+      ]
+    });
     /*
     ~chart
     `scatter--px-time--right`
@@ -292,24 +237,18 @@ export const mutations = {
       ]
     })
     */
-    ~chart
-    `scatter--py-time`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--py-time``scatter`({
+      datasets: [
         {
-          label: 'タッチ箇所(y軸)と所要時間',
-          data: results.map(({dy, py, t}) =>
-            ({
-              x: (100 / dy * py) + ~~(Math.random() * 5),
-              y: t > 5 ? 5 : t
-            })
-          )
+          label: "タッチ箇所(y軸)と所要時間",
+          data: results.map(({ dy, py, t }) => ({
+            x: (100 / dy) * py + ~~(Math.random() * 5),
+            y: t > 5 ? 5 : t
+          }))
         }
       ]
-    })
-/*
+    });
+    /*
     ~chart
     `scatter--py-time--right`
     `scatter`
@@ -347,45 +286,35 @@ export const mutations = {
     })
 */
 
-    ~chart
-    `scatter--px-dist`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--px-dist``scatter`({
+      datasets: [
         {
-          label: 'タッチ箇所(x軸)とタッチ誤差',
-          data: results.map(({dx, px, py, tx, ty}) =>
-            ({
-              x: (100 / dx * px) + ~~(Math.random() * 5),
-              y: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
-              ? 600
-              : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
-            })
-          )
+          label: "タッチ箇所(x軸)とタッチ誤差",
+          data: results.map(({ dx, px, py, tx, ty }) => ({
+            x: (100 / dx) * px + ~~(Math.random() * 5),
+            y:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+                ? 600
+                : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2)
+          }))
         }
       ]
-    })
+    });
 
-    ~chart
-    `scatter--py-dist`
-    `scatter`
-    ({
-      datasets:
-      [
+    ~chart`scatter--py-dist``scatter`({
+      datasets: [
         {
-          label: 'タッチ箇所(y軸)とタッチ誤差',
-          data: results.map(({dy, px, py, tx, ty}) =>
-            ({
-              x: (100 / dy * py) + ~~(Math.random() * 5),
-              y: Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
-              ? 600
-              : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2),
-            })
-          )
+          label: "タッチ箇所(y軸)とタッチ誤差",
+          data: results.map(({ dy, px, py, tx, ty }) => ({
+            x: (100 / dy) * py + ~~(Math.random() * 5),
+            y:
+              Math.sqrt((px - tx) ** 2 + (py - ty) ** 2) > 600
+                ? 600
+                : Math.sqrt((px - tx) ** 2 + (py - ty) ** 2)
+          }))
         }
       ]
-    })
+    });
     /*
     ~chart `myChart` `line`
     ({
@@ -421,35 +350,25 @@ export const mutations = {
         }]
     })
     */
-
-  },
-}
+  }
+};
 
 // chartレンダリングのラッパー
-const chart = id =>
-  ([type]) =>
-    data => 
-      new Chart(
-        ctx([id[0]]),
-        {
-          type,
-          data
-        }
-      )
+const chart = id => ([type]) => data =>
+  new Chart(ctx([id[0]]), {
+    type,
+    data
+  });
 
 //canvas取得
-const ctx = idName =>
-  id([[idName]]).getContext('2d')
+const ctx = idName => id([[idName]]).getContext("2d");
 
 // id取得
-const id = id =>
-  document.getElementById(id[0])
+const id = id => document.getElementById(id[0]);
 
 // 全ての結果を1つの配列にする
 const makeResultsToInALine = results =>
-  results.reduce((pre, cur) =>
-    [ 
-      ... pre,
-      ... Object.entries(cur).map(v => v[1])
-    ]
-  , [])
+  results.reduce(
+    (pre, cur) => [...pre, ...Object.entries(cur).map(v => v[1])],
+    []
+  );
